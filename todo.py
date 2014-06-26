6e8e0704f6c87593211a93531aa7e3c875b986e9
toxinman <vo.martin98@gmail.com>
2014-06-26 19:47:12 +0200

    Weitere Verbesserungen

--- todo.py
+++ todo.py
@@ -5,11 +5,12 @@ from sys import argv
 ABKUERZUNG_ZU_FACH = {"D": "Deutsch", "Ma": "Mathematik",
                       "Ru": "Russisch", "Eng": "Englisch"}
 
+AUFGABEN = load(open("aufgaben.txt"))
 
-def schnell_aufgabe_erstellen(datum, fach, thema, aufgaben):
+
+def schnell_aufgabe_erstellen(datum, fach, thema):
     """Schnelles Erstellen einer Aufgabe via Terminal"""
-    aufgaben.update(
-        {abkuerzung(fach): [str(datum_auswerten(datum)), thema, False]})
+    AUFGABEN[abkuerzung(fach)] = [str(datum_auswerten(datum)), thema, False]
 
 
 def neue_aufgabe():
@@ -18,7 +19,12 @@ def neue_aufgabe():
     datum = raw_input("Datum: ")
     thema = raw_input("Thema: ")
     note = bool(raw_input("Note? "))
-    return {abkuerzung(fach): [str(datum_auswerten(datum)), thema, note]}
+    return {abkuerzung_expandieren(fach): [str(datum_auswerten(datum)), thema, note]}
+
+
+def aufgabe_entfernen(fach):
+    """Entfernen einer bestehenden Aufgabe"""
+    del AUFGABEN[abkuerzung_expandieren(fach)]
 
 
 def abkuerzung_expandieren(fach):
@@ -28,61 +34,51 @@ def abkuerzung_expandieren(fach):
 
 def datum_auswerten(datum):
     """Datum wird zur Weiterverabeitung bearbeitet"""
-    if "." in datum:
+    try:
         # Deutsches Datum
-        datum = map(int, datum.split("."))
-        datum = datetime(datum[2], datum[1], datum[0])
-    else:
+        tag, monat, jahr = map(int, datum.split("."))
+    except:
         # Internationales Datum
-        datum = map(int, datum.split("-"))
-        datum = datetime(datum[0], datum[1], datum[2])
-    return datum
+        jahr, monat, tag = map(int, datum.split("-"))
+
+    return datetime(jahr, monat, tag)
 
 
-def aufgabe_entfernen(aufgaben, fach):
-    """Entfernen einer bestehenden Aufgabe"""
-    del aufgaben[fach]
+def countdown_berechnen(datum):
+    """Berechnen der noch verbleibenden Zeit"""
+    return (datum_auswerten(datum) - datetime.now()).days + 1
 
 
-def ausgabe_der_aufgaben(aufgaben):
+def ausgabe_der_aufgaben():
     """Ausgabe aller Aufgaben"""
-    print
-    for key, value in sorted(aufgaben.iteritems()):
-        # countdown = Tage von heute bis zum Datum
-        countdown = (datum_auswerten(value[0][:10]) - datetime.now()).days + 1
-        # 00:00:00 entfernen
-        value[0] = value[0][:10]
-
-        print "%s - noch %d Tag(e)" % (key, countdown)
+    for key, value in sorted(AUFGABEN.iteritems()):
+        print "%s - noch %d Tag(e)" % (key, countdown_berechnen(value[0][:10]))
         for eintrag in value:
             print eintrag
         print
-    print
 
 
 def main():
-    aufgaben = load(open("aufgaben.txt"))
-
+    """Hauptfunktion"""
     if argv[1:]:
-        # Wenn Paramter uebergeben wurden
         script, datum, fach, thema = argv
-        schnell_aufgabe_erstellen(datum, fach, thema, aufgaben)
-        dump(aufgaben, open("aufgaben.txt", "w"))
+        schnell_aufgabe_erstellen(datum, fach, thema)
+        dump(AUFGABEN, open("aufgaben.txt", "w"))
         exit()
 
     while True:
-        ausgabe_der_aufgaben(aufgaben)
+        ausgabe_der_aufgaben()
 
         print "Neue Aufgabe anlegen oder Aufgabe entfernen?"
         auswahl = raw_input("> ").lower()
 
         if "neu" in auswahl:
-            aufgaben.update(neue_aufgabe())
-            dump(aufgaben, open("aufgaben.txt", "w"))
+            AUFGABEN.update(neue_aufgabe())
+            dump(AUFGABEN, open("aufgaben.txt", "w"))
             print
         elif "entf" in auswahl:
             fach = raw_input("Welches Fach: ")
-            dump(aufgabe_entfernen(aufgaben, fach), open("aufgaben.txt", "w"))
+            dump(aufgabe_entfernen(fach), open("aufgaben.txt", "w"))
             print
         else:
             break
@@ -93,9 +89,7 @@ if __name__ == '__main__':
 
 # Was noch fehlt
 # ======
+# Aufgabe-Entfernen Funktion kaputt (alles wird geloescht)
 # Verhalten, wenn Datei leer
 # with Statement
 # Sortieren nach noch verbleibender Zeit
-# Aufgabe mit Erinnerung durch OSX verbinden
-# Online Zugang
-# GUI
